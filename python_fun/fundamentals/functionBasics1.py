5cf887a65f45505adfe70b762827e6e8ec349be2
Nancy Do <nancydo272@gmail.com>
2022-12-10 15:48:29 -0500

    functions and conditions

--- python_fun/fundamentals/functionBasics1.py
+++ python_fun/fundamentals/functionBasics1.py
@@ -64,15 +64,84 @@
 #100, 10
 
 #9
-def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-    if b<c:
-        return 7
-    else:
-        return 14
-    return 3
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 
 #7 , 14, 21
+
+#10
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+#8
+
+#11
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+
+#500, 500, 300, 500
+
+#12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+
+#500, 500, 300, 500
+#only 1 300 b/c its not print(foobar) so you are only returning 
+
+#13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+#500, 500, 300, 300
+
+#14
+# def foo():
+#     print(1)
+#     bar()
+#     print(2)
+# def bar():
+#     print(3)
+# foo()
+#1 ,3, 2
+
+#15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+#1, 3, 5, 10
 
