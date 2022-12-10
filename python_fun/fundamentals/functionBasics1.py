47d3976c87579e1a8c5a72a5d430fac808e38e50
Nancy Do <nancydo272@gmail.com>
2022-12-10 15:12:52 -0500

    none types b/c no return from function

--- python_fun/fundamentals/functionBasics1.py
+++ python_fun/fundamentals/functionBasics1.py
@@ -24,8 +24,23 @@
 #prediction: 5, only returns the first one. 
 
 #4
-def number_of_fingers():
-    return 5
-    print(10)
-print(number_of_fingers())
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
 #prediction:5 in the rerturn statement b/c its before the print statement  
+
+#5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)            #this print statement is outside the function when no aurgment was given
+#prediction: 5 and none
+
+#6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
+#prediction: 3 and 5 --> error b/c cannot add nonetypes
+#NoneType = function that does not return anything 
