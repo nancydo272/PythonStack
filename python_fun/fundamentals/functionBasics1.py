4460ece2b3e3ff7171c5c775f05fbb06c23a355e
Nancy Do <nancydo272@gmail.com>
2022-12-10 14:55:32 -0500

    function basics about code order

--- python_fun/fundamentals/functionBasics1.py
+++ python_fun/fundamentals/functionBasics1.py
@@ -1,9 +1,24 @@
 #For each of the following code snippets, predict the output 
 
 #1
-def number_of_food_groups():
-    return 5
-print(number_of_food_groups())
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
 
 #prediction: 5 
 #output: 5
+
+#2
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#prediction: Error b/c of undefined variable 
+#outcome: Error 
+
+#3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+
+#prediction: 5, only returns the first one. 
