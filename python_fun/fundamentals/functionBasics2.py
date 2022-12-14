fc3aaf7a63fa141d3ea241eb947c6a3b740f55f0
Nancy Do <nancydo272@gmail.com>
2022-12-13 21:35:37 -0500

    print and return from lists

--- python_fun/fundamentals/functionBasics2.py
+++ python_fun/fundamentals/functionBasics2.py
@@ -13,7 +13,10 @@ print(countDown(5))
 # 2.Print and Return - Create a function that will receive a list with two numbers. 
 # Print the first value and return the second.
     # Example: print_and_return([1,2]) should print 1 and return 2
-
+def print_and_return(arr):
+    print(arr[0])
+    return (arr[1])
+print(print_and_return([1,2]))
 # 3.First Plus Length - Create a function that accepts a list and 
 # returns the sum of the first value in the list plus the list's length.
     # Example: first_plus_length([1,2,3,4,5]) should 
