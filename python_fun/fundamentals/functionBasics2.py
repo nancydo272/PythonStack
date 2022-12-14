0cd1c841560ec84b3953ab07820c58706d4eaf04
Nancy Do <nancydo272@gmail.com>
2022-12-13 21:50:13 -0500

    arr[0] + len(arr)

--- python_fun/fundamentals/functionBasics2.py
+++ python_fun/fundamentals/functionBasics2.py
@@ -3,26 +3,26 @@
 # down to 0 (as the last element).
     #Example: countdown(5) should return [5,4,3,2,1,0]
 def countDown(num): 
-    arr = []
+    list = []
     for num in range(num, -1, -1):
-        arr.append(num)
-    return arr
+        list.append(num)
+    return list
 
 print(countDown(5))
 
 # 2.Print and Return - Create a function that will receive a list with two numbers. 
 # Print the first value and return the second.
     # Example: print_and_return([1,2]) should print 1 and return 2
-def print_and_return(arr):
-    print(arr[0])
-    return (arr[1])
+def print_and_return(list):
+    print(list[0])
+    return (list[1])
 print(print_and_return([1,2]))
 # 3.First Plus Length - Create a function that accepts a list and 
 # returns the sum of the first value in the list plus the list's length.
     # Example: first_plus_length([1,2,3,4,5]) should 
     # return 6 (first value: 1 + length: 5)
-def first_plus_length(arr):
-    sum = arr[0]+ len(arr)
+def first_plus_length(list):
+    sum = list[0]+ len(list)
     return sum
 
 print(first_plus_length([1,2,3,4,5]))
@@ -33,7 +33,9 @@ print(first_plus_length([1,2,3,4,5]))
     # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and 
     # return [5,3,4]
     # Example: values_greater_than_second([3]) should return False
+def values_greater_than_second(list):
 
+print(values_greater_than_second([5,2,3,2,1,4]))
 # 5.This Length, That Value - Write a function that accepts two integers as 
 # parameters: size and value. The function should create and return a 
 # list whose length is equal to the given size, and whose values are all the 
