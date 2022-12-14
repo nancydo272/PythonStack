53243fadb688bc0d3c1f3e7e3cbf6e9e63322eca
Nancy Do <nancydo272@gmail.com>
2022-12-13 21:26:00 -0500

    function bascis 2

--- python_fun/fundamentals/functionBasics2.py
+++ python_fun/fundamentals/functionBasics2.py
@@ -2,6 +2,13 @@
 # #Return a new list that counts down by one, from the number (as the 0th element) 
 # down to 0 (as the last element).
     #Example: countdown(5) should return [5,4,3,2,1,0]
+def countDown(num): 
+    arr = []
+    for num in range(num, -1, -1):
+        arr.append(num)
+    return arr
+
+print(countDown(5))
 
 # 2.Print and Return - Create a function that will receive a list with two numbers. 
 # Print the first value and return the second.
