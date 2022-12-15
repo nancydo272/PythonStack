e36e2fadfc08db1d32e7868bf2b77b3be5d971cd
Nancy Do <nancydo272@gmail.com>
2022-12-15 14:56:54 -0500

    updating dictionaries and lists

---
+++ python_fun/fundamentals/functionInterm.py
@@ -0,0 +1,26 @@
+#Avoid using class keywords like int, str, and dict as variable/parameters names
+#1. Update Values in Dictionaries and Lists
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+# In the sports_directory, change 'Messi' to 'Andres'
+# Change the value 20 in z to 30
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]= 15
+print(x)
+
+students[0]['lastname'] = 'Bryant'
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
